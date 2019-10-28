77d5c41a6cf86cd122bc025583be7e16cf0620e4
Léna <lena.clavier@gmail>
2019-10-28 16:07:08 +0100

    add database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -7,13 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @param ProductRepository $repository
      * @return Response
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
+        $products = $repository->findLatest();
         return $this->render('pages/home.html.twig', [
-            'current_menu' => 'home'
+            'products' => $products, 'current_menu' => 'home'
+        ]);
+    }
+
+    public function show(ProductRepository $repository): Response
+    {
+        $products = $repository->findLatest();
+        return $this->render('pages/home.html.twig', [
+            'products' => $products, 'current_menu' => 'home'
         ]);
     }
 }
